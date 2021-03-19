d3c0bf2e6c64e2244055ea9602bcf8a6b6ed8a78
Chris Richards <chris.richards@yellowfeather.co.uk>
2021-03-19 21:40:44 +0000

    Removes service worker

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,10 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
 
-import "./index.css";
+import './index.css'
 
-store.dispatch("init");
+store.dispatch('init')
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+createApp(App).use(store).use(router).mount('#app')
