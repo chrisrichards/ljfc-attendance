408c171311b22bb74089cfee6ee27b7e39f79fad
Chris Richards <chris.richards@yellowfeather.co.uk>
2021-03-20 14:35:38 +0000

    Fix cancel add / edit team route

--- src/views/teams/Edit.vue
+++ src/views/teams/Edit.vue
@@ -26,6 +26,14 @@
             <div class="pt-5">
               <div class="flex justify-end">
                 <router-link
+                  v-if="team.id === undefined"
+                  :to="{ name: 'Teams' }"
+                  class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Cancel
+                </router-link>
+                <router-link
+                  v-if="team.id != undefined"
                   :to="{ name: 'Players', params: { teamId: team.id } }"
                   class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
